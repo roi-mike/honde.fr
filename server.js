1e12eda986d7086feead04f02b31d9fa8e22052f
Maboza Hondi <dev.mabozahondi@gmail.com>
2021-05-12 17:58:02 +0200

    test start

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 
+const servePort = process.env.PORT || 8080;
+
 
 app.get('/',(req,res) => {
     res.sendFile(__dirname+'/views/index.html');
 });
 
-app.listen(8080);
+app.listen(servePort);
