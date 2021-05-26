1f5bacc77456f514c9bbdf4269195cbe648daa5a
Maboza Hondi <dev.mabozahondi@gmail.com>
2021-05-26 17:39:47 +0200

    merger le projet angular sur node

--- server.js
+++ server.js
@@ -3,9 +3,29 @@ const app = express();
 
 const servePort = process.env.PORT || 8080;
 
+app.use('/static',express.static('public/css'));
+app.use('/imgstatic',express.static('public/assets'));
+app.use(express.static('/public/css'));
+
 
 app.get('/',(req,res) => {
-    res.sendFile(__dirname+'/views/index.html');
+    res.sendFile(__dirname+'/views/index_view_component.html');
+});
+
+app.get('/accounts/login',(req,res) => {
+    res.sendFile(__dirname+'/views/login_view_component.html');
+});
+
+app.get('/accounts/emailsignup',(req,res) => {
+    res.sendFile(__dirname+'/views/register_view_component.html');
+});
+
+app.get('/accounts/password/reset',(req,res) => {
+    res.sendFile(__dirname+'/views/pwd_reset_view_component.html');
+});
+
+app.get('**',(req,res) => {
+    res.sendFile(__dirname+'/views/erreur_view_component.html');
 });
 
 app.listen(servePort);
