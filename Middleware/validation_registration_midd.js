d4b6b426b7a95e650645cee65cdb6288d6c64f6b
Maboza Hondi <dev.mabozahondi@gmail.com>
2021-06-10 16:57:30 +0200

    mise ajoure de l inscription

--- Middleware/validation_registration_midd.js
+++ Middleware/validation_registration_midd.js
@@ -4,14 +4,31 @@ const mongoose = require('mongoose');
 const validation_registration_midd = async (req, res, next) => {
 
     if(req.params.toke_validation_user){
-        console.log('OK VALIDATION ', req.params);
         toke_valid_user = req.params.toke_validation_user;
         //COLLECTION
         const { User } = require('../Models/User_db.js');
+        //UTILISE LES FONCTIONS DE MONGODB POUR MODIFIER
+        mongoose.set('useFindAndModify', false);
 
-        await User.findOneAndUpdate({toke_valid_user : toke_valid_user},{$set:{role:1}});
+        await User.findOneAndUpdate({toke_valid_user : toke_valid_user},{$set:{role:1}})
+        .exec()
+        .then(result => {
+            
+            if(result){
+                //Si il ya un resulta en fait NEXT()
+                console.log('RESULT : ', result);
+                next();
 
-        next();
+            }else{
+                //PAS DE RESULT DANS LA BDD RETOUR ACCUEIL
+                console.log('PAS RESULT : ', result);
+                res.redirect('/accounts/emailsignup');
+            }
+
+        })
+        .catch(erreur => {
+            console.log('ERREUR VALIDATION TOKEN : ',erreur)
+        })
     }else{
         res.redirect('/');
         next();
